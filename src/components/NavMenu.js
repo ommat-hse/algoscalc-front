6d5a5e5ae8d4725a1993eb491928ec941a249035
Трефилов Дмитрий Александрович <DATrefilov@ibs.ru>
2023-04-24 18:47:23 +0500

    Test. Обновил шапку сайта.

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,36 +1,34 @@
 import React from "react";
 import { Collapse, Navbar, NavbarBrand } from "reactstrap";
 import "./NavMenu.css";
-import mainLogo from "./img/Logo.jpg";
+import mainLogo from "./img/Logo.png";
 import { prodLink } from "../Api/index";
 
 export const NavMenu = () => {
 
     if(prodLink === "prod.ommat.ru")
-        document.title = "Калькуляторы"
+        document.title = "Онлайн калькулятор"
     else
-        document.title = "Калькуляторы (тест)"
+        document.title = "Онлайн калькулятор (тест)"
 
   return (
     <header>
       <Navbar
         className="navbar-expand-sm navbar-toggleable-sm border-bottom box-shadow mb-3"
-        style={{ backgroundColor: "#2888D5", color: "#FFFFFF" }}
+        style={{ backgroundColor: "#1B294F", color: "#FFFFFF" }}
       >
-        <img src={mainLogo} alt="OMMAT" width="40" height="40" />
+        <img src={mainLogo} alt="OMMAT" width="60" height="60" />
         <NavbarBrand
           style={{
             color: "#FFFFFF",
             fontWeight: "bold",
             paddingLeft: "10px",
             fontSize: "22px",
+            marginRight: "auto"
           }}
         >
-            { prodLink === "prod.ommat.ru" ? "Калькуляторы" : "Калькуляторы (тест)" }
+            { prodLink === "prod.ommat.ru" ? "Онлайн калькулятор" : "Онлайн калькулятор (тест)" }
         </NavbarBrand>
-        <Collapse className="flex-sm-row-reverse" navbar>
-            <span id="fullName-user">OMMAT</span>
-        </Collapse>
       </Navbar>
     </header>
   );
