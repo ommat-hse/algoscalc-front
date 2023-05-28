2d0cf85e5fcb9451956cf6e592f68a47338ad246
Трефилов Дмитрий Александрович <DATrefilov@ibs.ru>
2023-05-29 01:40:27 +0500

    Test. Добавил отображение ошибок в базовом модальном окошке.

--- src/components/Algorithm.js
+++ src/components/Algorithm.js
@@ -9,10 +9,16 @@ import telegramIcon from "./img/Telegram.png";
 import vkIcon from "./img/VK.png";
 import ourLogo from "./img/Logo.png";
 import {TextField} from "@mui/material";
+import ModalWindowDialog from "./ModalWindow/ModalWindowDialog";
 
 export const Algorithm = () => {
     const queryParameters = new URLSearchParams(window.location.search);
     const algorithmParametr = queryParameters.get("alg");
+
+    const [openDialogModal, setOpenDialogModal] = React.useState(false);
+    const [goHomeModal, setGoHomeModal] = React.useState(false);
+    const [wrongMessage, setWrongMessage] = React.useState("");
+
     const [algorithmTitle, setAlgorithmTitle] = React.useState("Загружаем алгоритм...");
     const [algorithmDescription, setAlgorithmDescription] = React.useState("Загружаем алгоритм...");
     const [redirectToHome, setRedirectToHome] = React.useState(false);
@@ -34,7 +40,8 @@ export const Algorithm = () => {
                 }
             )
             .catch((error) => {
-                alert(error);
+                setWrongMessage(error.message);
+                setOpenDialogModal(!openDialogModal);
             });
     };
 
@@ -48,14 +55,16 @@ export const Algorithm = () => {
                         }
                         else
                         {
-                            alert(res.data.errors);
-                            setRedirectToHome(true);
+                            setWrongMessage(res.data.errors);
+                            setGoHomeModal(true);
+                            setOpenDialogModal(!openDialogModal);
                         }
                     }
                 }
             )
             .catch((error) => {
-                alert(error);
+                setWrongMessage(error.message);
+                setOpenDialogModal(!openDialogModal);
             });
     };
 
@@ -64,60 +73,79 @@ export const Algorithm = () => {
         loadAlgorithm();
     }, []);
 
+    const redirectToHomePage = (home: Boolean) => {
+        if(home) {
+            setRedirectToHome(true);
+        }
+    };
+
   return (
-      <div style={{display: "flex", marginTop: "30px"}}>
-          {redirectToHome && (<Navigate to="/" />) }
-          <div style={{maxWidth: "25%", margin: "5px"}}>
-              <h4 style={{fontWeight: "bold", cursor: "pointer"}} onClick={() => {window.location.href = "#";}}>Главная</h4>
-              <Divider style={{marginBottom: "10px"}}></Divider>
-              <div style={{display: "grid"}}>
-                  {algorithmsHaving === "" && (<p>Готовим калькуляторы...</p>)}
-                  {algorithmsHaving !== "" && algorithmsHaving}
-              </div>
+      <>
+          <div>
+              {openDialogModal && (
+                  <ModalWindowDialog
+                      title="Произошла ошибка!"
+                      message={wrongMessage}
+                      onClose={(home: Boolean) => {setOpenDialogModal(!openDialogModal); redirectToHomePage(home);}}
+                      isActive={openDialogModal}
+                      goHome={goHomeModal}
+                  />
+              )}
           </div>
-          <div style={{maxWidth: "75%", margin: "5px", width: "100%"}}>
-              <div style={{marginLeft: "25px"}}>
-                  <h3>{algorithmTitle}</h3>
-                  <p>{algorithmDescription}</p>
+          <div style={{display: "flex", marginTop: "30px"}}>
+              {redirectToHome && (<Navigate to="/" />) }
+              <div style={{maxWidth: "25%", margin: "5px"}}>
+                  <h4 style={{fontWeight: "bold", cursor: "pointer"}} onClick={() => {window.location.href = "#";}}>Главная</h4>
+                  <Divider style={{marginBottom: "10px"}}></Divider>
+                  <div style={{display: "grid"}}>
+                      {algorithmsHaving === "" && (<p>Готовим калькуляторы...</p>)}
+                      {algorithmsHaving !== "" && algorithmsHaving}
+                  </div>
               </div>
-              <div>
-                  <Container style={{marginTop: "10px", paddingRight: "0px"}}>
-                      <Box sx={{ bgcolor: "#F6F6F6", padding: "15px"}}>
-                          <div><b>Средний расход топлива (л/100км)</b></div>
-                          <TextField fullWidth id="outlined-basic" label="Введите неотрицательное вещественное число" variant="outlined" style={{marginTop: "5px"}} />
-                          <div style={{marginTop: "5px"}}><b>Стоимость 1 л. топлива (руб)</b></div>
-                          <TextField fullWidth id="outlined-basic" label="Введите неотрицательное вещественное число" variant="filled" style={{marginTop: "5px"}} />
-                          <div style={{marginTop: "5px"}}><b>Сколько хотите проехать</b></div>
-                          <TextField fullWidth id="outlined-basic" label="Введите неотрицательное вещественное число" variant="standard" style={{marginTop: "5px"}} />
-                      </Box>
-                  </Container>
-              </div>
-              <div>
-                  <Container style={{marginTop: "10px", paddingRight: "0px"}}>
-                      <Box sx={{ bgcolor: "#AFB4C1", padding: "15px"}}>
-                          <div><b>Средний расход топлива (л/100км)</b></div>
-                          <TextField fullWidth id="outlined-basic" label="Введите неотрицательное вещественное число" variant="outlined" style={{marginTop: "5px"}} />
-                          <div style={{marginTop: "5px"}}><b>Стоимость 1 л. топлива (руб)</b></div>
-                          <TextField fullWidth id="outlined-basic" label="Введите неотрицательное вещественное число" variant="filled" style={{marginTop: "5px"}} />
-                          <div style={{marginTop: "5px"}}><b>Сколько хотите проехать</b></div>
-                          <TextField fullWidth id="outlined-basic" label="Введите неотрицательное вещественное число" variant="standard" style={{marginTop: "5px"}} />
-                      </Box>
-                  </Container>
-              </div>
-              <div style={{textAlign: "right", marginTop: "40px", marginBottom: "20px"}}>
-                  <p>Поделиться страницей в социальных сетях:</p>
+              <div style={{maxWidth: "75%", margin: "5px", width: "100%"}}>
+                  <div style={{marginLeft: "25px"}}>
+                      <h3>{algorithmTitle}</h3>
+                      <p>{algorithmDescription}</p>
+                  </div>
                   <div>
-                      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                      <a href={`https://t.me/share/url?url=${host}/algorithm?alg=${algorithmParametr}&text=Онлайн кальуляторы OMMAT.ru - ${algorithmTitle}`} target="_blank">
-                          <img src={telegramIcon} alt="Телеграм" style={{marginRight: "10px"}} />
-                      </a>
-                      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                      <a href={`https://vk.com/share.php?url=${host}/algorithm?alg=${algorithmParametr}&title=Онлайн кальуляторы OMMAT.ru - ${algorithmTitle}&noparse=true&image=${host}${ourLogo}`} target="_blank">
-                          <img src={vkIcon} alt="ВКонтакте"/>
-                      </a>
+                      <Container style={{marginTop: "10px", paddingRight: "0px"}}>
+                          <Box sx={{ bgcolor: "#F6F6F6", padding: "15px"}}>
+                              <div><b>Средний расход топлива (л/100км)</b></div>
+                              <TextField fullWidth id="outlined-basic" label="Введите неотрицательное вещественное число" variant="outlined" style={{marginTop: "5px"}} />
+                              <div style={{marginTop: "5px"}}><b>Стоимость 1 л. топлива (руб)</b></div>
+                              <TextField fullWidth id="outlined-basic" label="Введите неотрицательное вещественное число" variant="filled" style={{marginTop: "5px"}} />
+                              <div style={{marginTop: "5px"}}><b>Сколько хотите проехать</b></div>
+                              <TextField fullWidth id="outlined-basic" label="Введите неотрицательное вещественное число" variant="standard" style={{marginTop: "5px"}} />
+                          </Box>
+                      </Container>
+                  </div>
+                  <div>
+                      <Container style={{marginTop: "10px", paddingRight: "0px"}}>
+                          <Box sx={{ bgcolor: "#AFB4C1", padding: "15px"}}>
+                              <div><b>Средний расход топлива (л/100км)</b></div>
+                              <TextField fullWidth id="outlined-basic" label="Введите неотрицательное вещественное число" variant="outlined" style={{marginTop: "5px"}} />
+                              <div style={{marginTop: "5px"}}><b>Стоимость 1 л. топлива (руб)</b></div>
+                              <TextField fullWidth id="outlined-basic" label="Введите неотрицательное вещественное число" variant="filled" style={{marginTop: "5px"}} />
+                              <div style={{marginTop: "5px"}}><b>Сколько хотите проехать</b></div>
+                              <TextField fullWidth id="outlined-basic" label="Введите неотрицательное вещественное число" variant="standard" style={{marginTop: "5px"}} />
+                          </Box>
+                      </Container>
+                  </div>
+                  <div style={{textAlign: "right", marginTop: "40px", marginBottom: "20px"}}>
+                      <p>Поделиться страницей в социальных сетях:</p>
+                      <div>
+                          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                          <a href={`https://t.me/share/url?url=${host}/algorithm?alg=${algorithmParametr}&text=Онлайн кальуляторы OMMAT.ru - ${algorithmTitle}`} target="_blank">
+                              <img src={telegramIcon} alt="Телеграм" style={{marginRight: "10px"}} />
+                          </a>
+                          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                          <a href={`https://vk.com/share.php?url=${host}/algorithm?alg=${algorithmParametr}&title=Онлайн кальуляторы OMMAT.ru - ${algorithmTitle}&noparse=true&image=${host}${ourLogo}`} target="_blank">
+                              <img src={vkIcon} alt="ВКонтакте"/>
+                          </a>
+                      </div>
                   </div>
               </div>
           </div>
-      </div>
+      </>
   );
 };
